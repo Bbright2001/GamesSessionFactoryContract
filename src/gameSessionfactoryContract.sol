27ee54eeeb257e1890628306784a06f0dd6b896d
Bright Bassey <basseyu01@gmail.com>
2025-07-08 15:44:47 +0100

    ADD: added helper functions

--- src/gameSessionfactoryContract.sol
+++ src/gameSessionfactoryContract.sol
@@ -40,7 +40,7 @@ contract rockPaperScissorsGame{
             revealedMove: ""
         });
 
-        result = result.pending;
+        result = GameResult.pending;
     }
 
     function joinGame(address _player2) external{
@@ -50,17 +50,15 @@ contract rockPaperScissorsGame{
         player2 = Player({
             addr: _player2;
             committedMove: ,
-            hasCommited: false,
-            revealedMove ""
+            hasCommitted: false,
+            revealedMove: ""
             
         })
 
         bool hasStarted = true; 
     }
     function commitMoves(string memory _move, address _player) external {
-     
-
-        // bytes32 hash = keccak256(abi.encodePacked(_move, _secret));
+        require(validMove(_move), "Invalid Move")
 
         if(msg.sender == player1.addr){
 
@@ -80,9 +78,63 @@ contract rockPaperScissorsGame{
         }
 
         emit  moveCommitted(_player);
+
+        //Reveal player moves
+
+        revealMove(player1);
+        revealMove(player2);
        
     }
 
+    function validMove(string memory _move) internal pure returns (bool){
+        byte32 moveHash = keccak256(bytes(_move));
+
+        return (
+            moveHash == keccak256("rock") ||
+            moveHash == keccak256("scissors") ||
+            moveHash == keccak256("paper")
+        )
+    }
+
+    function revealMove(address player) internal returns (string){
+        if(player == player1.addr){
+
+            if( (player1.hasCommitted) )  revert mustCommit();
+            player1.revealedMove = player1.committedMove;
+
+            return player1.committedMove;
+        }else if( player == player.addr){
+            if( !(player2.hasCommitted) ) revert mustCommit();
+              player2.revealedMove = player2.committedMove;
+
+               return player2.committedMove;
+        } else {
+            "Invalid player"
+        }
+    }
+
+    function declareWinner() internal {
+        string memory move1 = player1.revealedMove;
+        string memory move2 = player2.revealedMove;
+
+        if(keccak256(move1) == keccak256(move2)){
+            result = GameResult.Draw;
+        } else if (
+            compare( (move1, "rock") && compare(move2, "scissors") ) ||
+            compare( (move1, "scissors") && compare(move2, "paper") ) ||
+            compare( (move1, "paper") && compare(move2, "rock") )
+        ){
+            result = GameResult.player1Wins;
+        } else {
+            result = GameResult.player2wins;
+        }
+
+        bool hasEnded = true;
+    }
+
+    function compare(string memory a, string memory b) internal pure returns (bool){
+        return keccak256(bytes(a)) == keccak256(bytes(b));
+    }
 }
 
 contract gameSessionFactory{
